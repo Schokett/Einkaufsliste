2ae8e6d32638d06761c2a9e227bbee866067c48c
schokett <skrollanelenazimmermann@gmail.com>
2026-07-08 14:57:04 +0200

    saved in localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,43 @@ import { Button } from "@/components/ui/button";
 import "./App.css";
 import { Input } from "@/components/ui/input";
 import { CheckCircleIcon } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
+interface Item {
+  id: string;
+  name: string;
+  amount: number;
+}
 
 function App() {
   const [input, setInput] = useState<string>("");
   const [inputNumber, setInputNumber] = useState<string>("");
 
+  // speichern
+  const [items, setItems] = useState<Item[]>(() => {
+    const savedItems = localStorage.getItem("shopping_list");
+    return savedItems ? JSON.parse(savedItems) : [];
+  });
+
   const isBtnEnabled = input.trim() !== "" && inputNumber.trim() !== "";
+
+  const handleAddItem = () => {
+    if (!isBtnEnabled) return;
+
+    const newItem: Item = {
+      id: crypto.randomUUID(),
+      name: input.trim(),
+      amount: Number(inputNumber),
+    };
+
+    const updatedItems = [newItem, ...items];
+    setItems(updatedItems);
+
+    localStorage.setItem("shopping_list", JSON.stringify(updatedItems));
+
+    setInput("");
+    setInputNumber("");
+  };
 
   return (
     <>
@@ -29,21 +59,25 @@ function App() {
             type="number"
           />
         </div>
-        <Button disabled={!isBtnEnabled} size={"lg"} className={"w-full"}>
+        <Button disabled={!isBtnEnabled} size={"lg"} className={"w-full"} onClick={handleAddItem}>
           Eintrag Hinzufügen
         </Button>
 
         <div className="flex flex-col w-full gap-2 mt-6">
-          <div className="border bg-card rounded-xl p-5 text-card-foreground flex justify-between items-center">
-            <div className="items-start flex flex-col">
-              <h3 className="text-lg font-semibold">Produkt{}</h3>
-              <p className="text-sm text-muted-foreground">Anzahl:{}</p>
+          {items.map((item) => (
+            <div
+              key={item.id}
+              className="border bg-card rounded-xl p-5 text-card-foreground flex justify-between items-center">
+              <div className="items-start flex flex-col">
+                <h3 className="text-lg font-semibold">{item.name}</h3>
+                <p className="text-sm text-muted-foreground">{item.amount}</p>
+              </div>
+              <Button size={"lg"} variant={"outline"}>
+                <CheckCircleIcon />
+                Abhaken
+              </Button>
             </div>
-            <Button size={"lg"} variant={"outline"}>
-              <CheckCircleIcon />
-              Abhaken
-            </Button>
-          </div>
+          ))}
         </div>
       </section>
     </>
